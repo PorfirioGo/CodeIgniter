e349221f561548dfb483fef5a616e440c2c758b5
Porfirio <gonzalez_porfirio@hotmail.com>
2023-10-31 12:05:12 -0600

    Se agrega Clase Codigofacilito, constructor padre y se cargan helpers

--- application/views/cursos/New.php
+++ application/views/cursos/New.php
@@ -1,5 +1,24 @@
 <?php 
 
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Codigofacilito extends CI_Controller { /*Lo primero que se tiene que tener dentro del la Class es un constructor*/
+
+	/*Inicio del Constructor. En PHP5 el constructor se escribe con 2 guiones bajos __ seguido de construct()*/
+	function __construct(){ /*Funcion Constructor*/
+
+	/*Una de las cosas que se tienen que ejecutar siempre es el Constructor  PADRE, para que las cosas funcionen como deberian y no tener ningun problema ala hora de trabajar dentro del CONTROLADOR */
+
+		/*Constructor Padre*/
+
+		parent::__construct(); /*Constructor Padre*/
+		
+		$this->load->helper('mihelper'); /*Se carga el helper, llamado mihelper, debajo de la ejecución del CONSTRUCTOR PADRE*/ 
+		
+		$this->load->helper('form'); /*Se carga el helper (form_helper.php) ruta application/system/helpers, ese helper tiene las funciones que vamos a estar utilizando*/
+
+		$this->load->model('codigofacilito_model'); /*Se carga el modelo('codigofacilito_model')*/ 
+	}
 
 
 
