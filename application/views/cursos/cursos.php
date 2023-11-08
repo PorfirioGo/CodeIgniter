0165b4ccf7454a4cdd2e5713bf4be67fb004304f
porfirio-confortequis <porfirio@confortequis.com>
2023-11-08 14:47:20 -0600

    Se agrega un link en el archivo de cursos.php, pero manda error

--- application/views/cursos/cursos.php
+++ application/views/cursos/cursos.php
@@ -4,10 +4,8 @@
 
 /*Ciclo foreach para mostrar los resutados obetenidos de la BD */
 
-	foreach ($cursos->result() as $curso){ /*Los metodos que tiene el objeto query es return, cuando hicimos la consulta que es el result, los resultados que obtuvo de la BD, todo eso almacenado en la variable $cursos accedemos al metodo result y reiteramos entre cada uno de ellos con una variable  que vamos a llamar $curso */
- 
- ?>
-	
+	foreach ($cursos->result() as $curso) { ?> <!--Los metodos que tiene el objeto query es return, cuando hicimos la consulta que es el result, los resultados que obtuvo de la BD, todo eso almacenado en la variable $cursos accedemos al metodo result y reiteramos entre cada uno de ellos con una variable  que vamos a llamar $curso-->
+
 	<!--Se crea lista no ordenada, para colocar los nombre de los cursos-->	
 		<ul>	<!--Se agrega un link para poder ir navegando entre cada uno de los cursos, 	-->		
 			<li><a href="<?= $curso->idCurso; ?>"><?= $curso->NombreCurso; ?></a></li> <!--Se agrega atributo NombreCurso(Nombre del campo de la BD)-->
@@ -16,8 +14,8 @@
 
 	}else{/**Se coloca un condicional else */
 		echo "<p>Error en la aplicación</p>";/*Se coloca una alerta */
-
-	}	
+	
+	}
 
   ?>
 	</body>
